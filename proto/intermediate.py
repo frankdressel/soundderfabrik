780576ae05996dac5cc4571a26a61255e5673b23
Frank Dressel <frankdressel@users.noreply.github.com>
2018-06-26 23:17:53 +0200

    Added lengths and note names.

--- proto/intermediate.py
+++ proto/intermediate.py
@@ -1,49 +1,9 @@
 # coding: utf-8
-from music21 import *
+import collections
 import os
 import requests
 import tempfile
 
-environment.set('autoDownload', 'allow')
-
-exampleUrl="http://www.mutopiaproject.org/ftp/BachJS/BWV208/Sheep/Sheep.mid"
-
-# First approach: Problems with non-existing triplets.
-s=converter.parse(exampleUrl)
-parts=[p.makeMeasures() for p in s.getElementsByClass(stream.Part)]
-print("Anzahl der Stimmen", len(parts))
-
-for n in list(parts[0].measures(0, 3).flat.getElementsByClass([note.Note, note.Rest])):
-     if n.isNote:
-         print(n, n.name, n.pitch.name, n.duration.fullName)
-     if n.isRest:
-         print("Rest", "-", n.duration.fullName)
-
-# Second approach: No problems with triplets but totally wrong
-r = requests.get(exampleUrl)
-with tempfile.NamedTemporaryFile() as f:
-    f.write(r.content)
-
-    fp=f.name
-    mf = midi.MidiFile()
-    mf.open(str(fp))
-    mf.read()
-    mf.close()
-
-    for i in range(len(mf.tracks)):
-        print("Track {}".format(i))
-        t=midi.translate.midiTrackToStream(mf.tracks[1], 96)
-        for n in list(t.flat.getElementsByClass([note.Note, note.Rest])):
-            if n.isNote:
-                print(n.name, n.pitch.name, n.duration.fullName)
-            if n.isRest:
-                print("Rest", "-", n.duration.fullName)
-            if n.isChord:
-                print([n for n in element.normalOrder])
-
-    f.close()
-
-# Third approach: More complicated but correct
 import ly.document
 import ly.music
 doc=ly.document.Document.load('/home/vagrant/Downloads/Sheep.ly')
@@ -51,4 +11,11 @@ d=ly.music.document(doc)
 
 r0=[r for r in d.find(ly.music.items.Relative)][0]
 c0=[i for i in r0.find(ly.music.items.Command)][0]
-[n for n in r0.find((ly.music.items.Note, ly.music.items.Duration))][0:20]
+elements=[n for n in r0.find((ly.music.items.Note, ly.music.items.Duration, ly.music.items.PipeSymbol, ly.music.items.Command, ly.music.items.KeySignature))][0:20]
+
+note=[]
+
+for elem in elements:
+    if type(elem)==ly.music.items.Note:
+        note.append({'name': elem.pitch.output(), 'length': '{}/{}'.format(elem.length().numerator, elem.length().denominator)})
+print(note)
